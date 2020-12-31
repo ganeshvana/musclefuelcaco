c8e62a856cbaeb48d68d290b2c6b90b07e1f4754
ramya-itara <ramya@itarait.com>
2020-12-31 18:44:29 +0530

    311220

---
+++ oi_master_extend/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Create & Edit Access Disable",
+    'summary': """Base form create and edit disable""",
+    "version": "13.0.1.0",
+    "category": 'Base, Purchase, Sale, Inventory, Accounting',
+    'author': "Odoo Implementers Private Limited",
+    "website": "http://www.odooimplementers.com",
+    'license': 'AGPL-3',
+    "depends": ['purchase','sale','base','sale_management','stock','account'],
+    "data": ['views/master_views.xml'
+
+    ],
+    "application": False,
+    'installable': True,
+}

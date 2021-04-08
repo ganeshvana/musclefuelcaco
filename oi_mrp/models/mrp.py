9dc8db7b22fee22c7ae5edd1a5c3f29fdbfd312f
ramya-itara <ramya@itarait.com>
2021-04-08 14:57:26 +0530

    mrp_update

--- oi_mrp/models/mrp.py
+++ oi_mrp/models/mrp.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api
+import requests
+from ast import literal_eval
 
 
 class ProductCategory(models.Model):
     _inherit = 'mrp.production'
 
     def get_data_from_musclefuel(self):
+        
         base_url = self.env['ir.config_parameter'].sudo().get_param('musclefuel.api_url')
         jwt_token = "3726fa0d2ae0550da60b39a42dc4408c"
         date = str(fields.Date.today())
         headers = {"given_date": date,
                    "secret_key": jwt_token}
 
-        url = base_url + 'oodoOrderDetailsperdate' + headers
-        r = requests.put(url, headers=headers, timeout=30)
+        url = base_url + 'oodoOrderDetailsperdate' + str(headers)
+        r = requests.put(url, timeout=30)
         result = literal_eval(r.text)
         if 'data' in result:
             print(result['data'])
@@ -25,3 +28,4 @@ class ProductCategory(models.Model):
                 }
                 MO = self.env['mrp.production'].sudo().create(data)
                 print(MO.name)
+

5704c37dac54449d97b4b0fc113c5890528e10f7
ramya-itara <ramya@itarait.com>
2021-05-26 10:19:43 +0530

    260521

--- oi_mrp/wizard/mrp_order_fetch.py
+++ oi_mrp/wizard/mrp_order_fetch.py
@@ -37,13 +37,36 @@ class MRPOrders(models.TransientModel):
             for line in result['data']:
                 product_obj = self.env['product.product'].search([('default_code', '=', line['productId'])])
                 if product_obj:
+                    bom = self.env['mrp.bom']._bom_find(product=product_obj, bom_type='normal')
+                    if bom:
+                        bom_id = bom.id
+                    else:
+                        bom_id = False
                     data = {
+                        'bom_id': bom_id,
                         'product_id': product_obj.id,
+                        'date_planned_start': date,
                         'product_uom_id': product_obj.uom_id.id,
                         'product_qty': int(line['cnt'])
                     }
                     MO = self.env['mrp.production'].sudo().create(data)
                     print(MO.name)
+                    if MO.bom_id and MO.product_qty > 0:
+                        # keep manual entries
+                        list_move_raw = [(4, move.id) for move in
+                                         MO.move_raw_ids.filtered(lambda m: not m.bom_line_id)]
+                        moves_raw_values = MO._get_moves_raw_values()
+                        move_raw_dict = {move.bom_line_id.id: move for move in
+                                         MO.move_raw_ids.filtered(lambda m: m.bom_line_id)}
+                        for move_raw_values in moves_raw_values:
+                            if move_raw_values['bom_line_id'] in move_raw_dict:
+                                # update existing entries
+                                list_move_raw += [
+                                    (1, move_raw_dict[move_raw_values['bom_line_id']].id, move_raw_values)]
+                            else:
+                                # add new entries
+                                list_move_raw += [(0, 0, move_raw_values)]
+                        MO.move_raw_ids = list_move_raw
                 else:
                     pass
             return True
